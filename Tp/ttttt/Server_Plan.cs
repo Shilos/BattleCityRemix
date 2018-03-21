c032d7af4d2a80f9f9d441ef0c5eda52c4ada166
Pavel Rem <PavelRemStep@gmail.com>
2018-03-21 13:22:38 +0300

    first commit

---
+++ Tp/ttttt/Server_Plan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Plan
+{
+	interface IServer
+	{
+		short Port { get; set; }
+		IHostListen hostListen { get; set; }
+		IEnumerable<IRoom> rooms { get; set; }
+		void Send_Room_List();
+	}
+
+	interface IRoom
+	{
+		IMesegeQueue mesegeQueue { get; set; }
+		IEngine engine { get; set; }
+		ISender sender { get; set; }
+		IEnumerable<IGamer> gamer { get; set; }
+		IroomStat roomStat { get; set; }
+	}
+
+	interface IGamer
+	{
+		IGamerStat gamerStat { get; set; }
+		UdpClient client { get; set; }
+	}
+
+	interface IEngine
+	{
+		//обработка полета пули
+		//отправка координат всех обьектов каждому игроку
+		//обработка события убийства
+		//начало игры
+		//конец игры
+	}
+
+	interface IHostListen { }
+}
---
+++ Tp/ttttt/common.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plan
+{
+		interface IGamerStat
+		{
+			string id_name { get; set; }
+			int lives { get; set; }
+			Position position { get; set; }
+			Direction direction { get; set; }
+			Teem teem { get; set; }
+			Position bullet_position { get; set; }
+		}
+
+		interface IroomStat
+		{
+			string id { get; set; }
+			int players_count { get; set; }
+			string creator_player_name { get; set; }
+		}
+
+		interface ISender { }
+
+		interface IMesegeQueue { }
+
+		struct Position { float x; float y; }
+
+		enum Direction { Left, Right, Up, Down }
+
+		enum Teem { Red, Green }
+}
