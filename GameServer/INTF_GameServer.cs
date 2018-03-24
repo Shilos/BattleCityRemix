91f8dba2620fd029db8fb1408c50182782f0ac1a
iMAK73 <muginovak73@gmail.com>
2018-03-24 19:37:30 +0200

    MAK_240318-1

---
+++ GameServer/INTF_GameServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Tanki
+{
+
+    public interface IProtoclol
+    {
+
+    }
+
+    public interface ISender
+    {
+
+    }
+
+    public interface IGamer
+    {
+        String id { get; set; }
+        Socket Socket { get; set; }
+    }
+
+    public interface IGameRoom
+    {
+        String RoomId { get; set; }
+        Socket RoomListner { get; }
+        IEnumerable<IGamer> Gamers { get; }
+        void AddGamer(IGamer newGamer);
+
+        IMessageQueue MessageQueue { get; }
+        ISender Sender { get; }
+        void RUN();
+
+    }
+
+
+    public interface IMessageQueue
+    {
+        IEnumerable<IProtoclol> Queue {get;}
+        Timer Timer { get; }
+        void Enque(IProtoclol msg);
+        void RUN();
+
+    }
+
+    public interface IServer
+    {
+        Socket ServerListner { get; }
+        IEnumerable<IGameRoom> Rooms { get; }
+
+
+    }
+}
