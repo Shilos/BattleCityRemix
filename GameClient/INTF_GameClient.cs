fc8e312f738a38527337ef45e7442bbe1aee1a55
Shilos <combahshilos@gmail.com>
2018-03-28 21:03:08 +0300

    1-st Commit

---
+++ GameClient/INTF_GameClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanki;
+
+namespace Tanki
+{
+    /// <summary>
+    /// Cущность отправляющая информацию от клиента хосту
+    /// </summary>
+    public interface ISender
+    {
+        string RemoteAdress { get; set; }   // ip хоста
+        int RemotePort { get; set; }        // порт хоста
+        IPackage Pack { get; set; }         // пакет на отправку
+        void SendMessage();
+    }
+
+
+    /// <summary>
+    /// Cущность принимающая информацию клиентом от хоста
+    /// </summary>
+    public interface IReceiver
+    {
+        bool Alive { get; set; }   // работает ли поток на прием
+        int LocalPort { get; set; }        // прослушивающий порт
+        IPackage Run();
+    }
+
+}
